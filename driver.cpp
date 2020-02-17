fcdfefed726b7c5e4c223b384bf9f60db0eac302
wylieza <wylieza@gmail.com>
2020-02-17 09:38:54 +0200

    Created the driver class and the associated header. Currently displays hello world

---
+++ driver.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <fstream>
+
+#include "driver.h"
+
+
+int main(void){
+    std::cout << "Hello, World!\n";
+};
---
+++ driver.h
@@ -0,0 +1,17 @@
+#ifndef DATABASE_H
+#define DATABASE_H
+
+//Define namespace
+namespace WYLJUS002{
+    void add_student(std::string name, std::string surname, std::string student_no, std::string, std::string class_record);
+
+    //Read
+
+    //Save
+
+    //Display
+
+    //Grade
+}
+
+#endif
