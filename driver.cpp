d0ac28971083ad11f821f61cd8a498009b56e5cd
wylieza <wylieza@gmail.com>
2020-02-17 21:01:40 +0200

    Created a menu and a loop to detect chosen item and when to quite

--- driver.cpp
+++ driver.cpp
@@ -3,7 +3,31 @@
 
 #include "driver.h"
 
+std::string menu[8] = {"\n>>>> MAIN MENU <<<<\n", "0: Add student",
+                        "1: Read database", "2: Save database",
+                        "3: Display given student data", 
+                        "4: Grade student", "q: Quite\n",
+                        "Enter a number (or q to quit) and press return..."};
+
+char menu_selection;
 
 int main(void){
-    std::cout << "Hello, World!\n";
+
+    for(;;){
+        //Print menu out
+        for (int i = 0; i < sizeof(menu)/sizeof(*menu); i++){
+            std::cout << menu[i] << "\n";
+        }
+
+        //Process input
+        std::cin >> menu_selection;
+        if (std::isdigit(menu_selection)){
+            std::cout << menu_selection;
+        }else{
+            return 0;
+        }
+
+        
+    }
 };
+
