b8271fd1c537e42c5ecc264c76745983effae0ab
wylieza <wylieza@gmail.com>
2020-02-18 09:49:14 +0200

    Creared a database tools application to interface with the database, the driver will interact through these two files

---
+++ database_tools.cpp
@@ -0,0 +1,28 @@
+
+include "database_tools.h"
+
+namespace WYLJUS002{
+
+    std::vector std::string student_records; //Prehaps define this in the header class?
+
+    //Functions
+    void add_student(){
+    std::cout << "Add student function\n";
+    }
+
+    void read_database(){
+        std::cout << "Read from database\n";
+    }
+
+    void save_database(){
+        std::cout << "Save the database\n";
+    }
+
+    void disp_student(){
+        std::cout << "Display student data\n";
+    }
+
+    void grade_student(){
+        std::cout << "Grade a student\n";
+    }
+}
---
+++ database_tools.h
@@ -0,0 +1,13 @@
+
+namespace WYLJUS002{
+
+    struct student_record{
+            std::string name;
+            std::string surname;
+            std::string student_no;
+            std::string class_record;
+        }
+
+    //Define functions
+
+}
